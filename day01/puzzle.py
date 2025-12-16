52fc4459602a706fd66ecb8483b9c241b2098e2f
Matt Free <mjfree34@gmail.com>
2025-12-16 12:21:55 -0500

    Day 1 part 2 complete

--- day01/puzzle.py
+++ day01/puzzle.py
@@ -14,18 +14,34 @@ def parse_input(input: str) -> [int]:
 
 def solve_part1(rotations: [int]) -> int:
     position = initial_position
-    num_zeroes = 0
+    num_exact_zeroes = 0
 
     for rotation in rotations:
         position += rotation
         position %= 100
         if position == 0:
-            num_zeroes += 1
+            num_exact_zeroes += 1
 
-    return num_zeroes
+    return num_exact_zeroes
+
+def solve_part2(rotations: [int]) -> int:
+    position = initial_position
+    num_click_zeroes = 0
+    
+    for rotation in rotations:
+        # Move one step at a time (brute force is fine)
+        for _ in range(abs(rotation)):
+            position += 1 if rotation > 0 else -1
+            position %= 100
+            if position == 0:
+                num_click_zeroes += 1
+    
+    return num_click_zeroes
 
 if __name__ == "__main__":
     with open("day01/input.txt", "r") as file:
         rotations = parse_input(file.read())
 
-    print("Part 1: ", solve_part1(rotations))
+    # print("Part 1: ", solve_part1(rotations))
+
+    print("Part 2: ", solve_part2(rotations))
