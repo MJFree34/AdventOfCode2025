aa733939cc6f985c4decc45213fde10c33ce3dbc
Matt Free <mjfree34@gmail.com>
2025-12-17 00:43:25 -0500

    Day 3 part 2 completed

--- day03/puzzle.py
+++ day03/puzzle.py
@@ -20,8 +20,31 @@ def solve_part1(input):
 
     return tot_out_joltage
 
+def solve_part2(input):
+    tot_out_joltage = 0
+
+    for bank in input:
+        to_drop = len(bank) - 12
+        stack = []
+
+        for digit in bank:
+            while stack and to_drop > 0 and stack[-1] < digit:
+                stack.pop()
+                to_drop -= 1
+            stack.append(digit)
+
+        result = ''.join(stack[:12])
+
+        # print(f"Bank: {bank}, Top 12: {result}")
+
+        tot_out_joltage += int(result)
+
+    return tot_out_joltage
+
 if __name__ == "__main__":
     with open("day03/input.txt", "r") as file:
         input = parse_input(file.read())
 
     print("Part 1:", solve_part1(input))
+
+    print("Part 2:", solve_part2(input))
