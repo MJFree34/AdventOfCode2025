2b767b60fbcb5bc13a125b32c9f4a1b44158c831
Matt Free <mjfree34@gmail.com>
2025-12-21 00:37:14 -0500

    Day 7 part 2 completed

--- day07/puzzle.py
+++ day07/puzzle.py
@@ -3,7 +3,7 @@ def parse_input(input):
 
 # def print_grid(grid):
 #     for row in grid:
-#         print(''.join(row))
+#         print(''.join(str(row)))
 
 def solve_part1(grid):
     # print("---Initial---")
@@ -33,8 +33,35 @@ def solve_part1(grid):
 
     return tot_splits
 
+def solve_part2(grid):
+    timeline_grid = [[0 for _c in row] for row in grid]
+
+    # Initialize bottom row with 1's
+    for j, c in enumerate(grid[-1]):
+        if c == '|':
+            timeline_grid[-1][j] = 1
+
+    for i in reversed(range(len(grid) - 1)):
+        # Propogate beams upwards
+        for j, c in enumerate(grid[i]):
+            if c == '|' or c == 'S':
+                timeline_grid[i][j] = timeline_grid[i + 1][j]
+
+        # Calculate splitters
+        for j, c in enumerate(grid[i]):
+            if c == '^':
+                left_count = timeline_grid[i + 1][j - 1] if j > 0 else 0
+                right_count = timeline_grid[i + 1][j + 1] if j < len(grid[i]) - 1 else 0
+                timeline_grid[i][j] = left_count + right_count
+    
+    # print_grid(timeline_grid)
+
+    return timeline_grid[0][grid[0].index('S')]
+
 if __name__ == "__main__":
     with open("day07/input.txt", "r") as file:
         input = parse_input(file.read())
  
     print("Part 1:", solve_part1(input))
+
+    print("Part 2:", solve_part2(input))
